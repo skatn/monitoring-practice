714f35079eea0252013944a0acb2aec1a316e985
skatn <87296704+skatn@users.noreply.github.com>
2023-10-29 21:04:58 +0900

    Initial commit

---
+++ src/main/java/com/example/monitoringpractice/MonitoringPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.monitoringpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringPracticeApplication.class, args);
+    }
+
+}
